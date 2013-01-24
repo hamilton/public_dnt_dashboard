4044f2e4d265f2be2b40008b5962c569711962dc
Ali Almossawi <almossawi@gmail.com>
2013-01-23 22:15:50 -0800

    You can now switch between granularities and platforms

--- js/global.js
+++ js/global.js
@@ -16,28 +16,54 @@ $(document).ready(function () {
 	});
 });
 
+//TODO clean up; put in separate function
 function assignEventListeners() {
-	$("#ff").on("click", function() {
+	$("#desktop").on("click", function() {
+		shift_selected("desktop", "platform");
 		desktop_or_mobile = "ff";
 		drawCharts(desktop_or_mobile + "_dnt_perc_" + date_granularity + ".json");
 	});
 	
-	$("#fennec").on("click", function() {
+	$("#mobile").on("click", function() {
+		shift_selected("mobile", "platform");
+		
 		desktop_or_mobile = "fennec";
 		drawCharts(desktop_or_mobile + "_dnt_perc_" + date_granularity + ".json");
 	});
 	
 	$("#dnt_perc_daily").on("click", function() {
+		shift_selected("daily", "granularity");
+		date_granularity = "daily";
 		drawCharts(desktop_or_mobile + "_dnt_perc_daily.json");
 	});
 	
 	$("#dnt_perc_weekly").on("click", function() {
+		shift_selected("weekly", "granularity");
+		date_granularity = "weekly";
 		drawCharts(desktop_or_mobile + "_dnt_perc_weekly.json");
 	});
 	
 	$("#dnt_perc_monthly").on("click", function() {
+		shift_selected("monthly", "granularity");
+		date_granularity = "monthly";
 		drawCharts(desktop_or_mobile + "_dnt_perc_monthly.json");
 	});
+}
+
+function shift_selected(option, platform_or_granularity) {
+	if(platform_or_granularity == "granularity") {
+		$("#dnt_perc_daily").html("DAILY");
+		$("#dnt_perc_weekly").html("WEEKLY");
+		$("#dnt_perc_monthly").html("MONTHLY");
+
+		$("#dnt_perc_" + option).html("<span class='selected_option'>" + option.toUpperCase() + "</span>");
+	}
+	else if(platform_or_granularity == "platform") {
+		$("#desktop").html("DESKTOP");
+		$("#mobile").html("MOBILE");
+		
+		$("#" + option).html("<span class='selected_option'>" + option.toUpperCase() + "</span>");
+	}
 }
 
 function drawCharts(json) {console.log(json);
