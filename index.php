04b2acfb268ba24753f64e621737b9128a4e8198
Ali Almossawi <almossawi@gmail.com>
2013-04-03 17:12:58 -0700

    Removed FB code

--- index.php
+++ index.php
@@ -67,29 +67,6 @@ header("Pragma: no-cache");
 </script>
 	</head>
 	<body>
-		<!--<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-38360687-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>-->
-
-		<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=337561759677631";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 		<a href="http://www.mozilla.org/" id="tabzilla">mozilla</a>
 		<img src="images/fxlogo_top.png" id="top_logo" />
 		<div id="unsupported">
