922663354e6dfb0e60962235c16d8fdec1fa0d03
anuragphadke <anurag.phadke@gmail.com>
2013-04-25 14:04:11 -0700

    Update index.php
    removing cache control as per cshields suggestion

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $dnt_enabled = array_key_exists('HTTP_DNT', $_SERVER);
 
 // Force no-caching
 header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
 header("Pragma: no-cache");
 ?>
 <!DOCTYPE html>
